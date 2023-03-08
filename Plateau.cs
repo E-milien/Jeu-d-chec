48a94d4e29a714bfc7d1e3466d9c7fa374e348eb
Émilien <99119837+E-milien@users.noreply.github.com>
2023-03-08 21:43:24 +0100

    Add files via upload

---
+++ Plateau.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace jeu_d_echec
+{
+    public class Plateau
+    {
+        private int[,] tab;
+        private List<Piece> lesPieces;
+
+        public Plateau()
+        {
+        }
+
+        public int[,] Tab { get => tab;
+            set
+            {
+                int[,] tmp = new int[8, 8];
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int y = 0; y < 8; y++)
+                    {
+                        tmp[i, y] = 0;
+                    }
+                }
+                tab = tmp;
+            }
+        }
+
+        public List<Piece> LesPieces { get => lesPieces; set => lesPieces = value; }
+
+        public void AjoutPiece(Piece unePiece)
+        {
+            LesPieces.Add(unePiece);
+        }
+        public void SeFaitManger(Piece unePiece)
+        {
+            LesPieces.Remove(unePiece);
+        }
+        public Piece Retrouver(int numPiece)
+        {
+            foreach (Piece p in LesPieces)
+            {
+                if (p.NumPiece == numPiece)
+                    return p;
+            }
+            return new Piece();
+        }
+        public Piece Retrouver(int x, int y)
+        {
+            foreach (Piece p in LesPieces)
+            {
+                if (p.PositionX == x && p.PositionY == y)
+                    return p;
+            }
+            return new Piece();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace jeu_d_echec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initioalisaton
+            Plateau p = new Plateau();
+            for (int i = 0; i < 8; i++)
+            {
+                p.AjoutPiece(new Pion(1, i, 1, true));
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                p.AjoutPiece(new Pion(1, i, 6, false));
+            }
+            //fin initialisation
+
+            while (true)
+            {
+                Console.WriteLine("quel est la case de la piece que vous voulez changer ? (X9)"); // quelle piece
+                string rep = Console.ReadLine();
+                int xPiece = char.ToUpper(rep[0]) - 64;
+                int yPiece = int.Parse(rep.Substring(1, 1));
+                Console.WriteLine("quel est la case ou vpous voulez aller ? (X9)"); //ou se deplacer
+                string rep2 = Console.ReadLine();
+                int xDep = char.ToUpper(rep[0]) - 64;
+                int yDep = int.Parse(rep.Substring(1, 1));
+                //recherche quelle piece et quel type
+                if (p.Retrouver(xPiece, yPiece) is Pion)
+                    ((Pion)p.Retrouver(xPiece, yPiece)).PeutSeDeplacer(xDep, yDep, p);
+            }
+        }
+    }
+}
+
